143407bd2630774c74f940e51e1bb74dbe3ea8fe
Arellys Villena <arellysvgamboa@gmail.com>
2026-06-20 14:37:47 -0500

    refactor: actualizar clase Cafe para los tamaños

--- src/Bebidas/Cafe.java
+++ src/Bebidas/Cafe.java
@@ -2,9 +2,13 @@ package Bebidas;
 
 public abstract class Cafe {
 
+    protected String tamano;
+
+    public Cafe(String tamano) {
+        this.tamano = tamano;
+    }
+
     public abstract String getDescripcion();
 
     public abstract double getCosto();
-
 }
-
